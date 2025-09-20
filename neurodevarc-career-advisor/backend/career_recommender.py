d2fcebb9f92becd10c30d2cf9fab8c90b5853099
devu1210 <155282523+devu1210@users.noreply.github.com>
2025-09-21 03:45:38 +0530

    Update career_recommender.py

--- neurodevarc-career-advisor/backend/career_recommender.py
+++ neurodevarc-career-advisor/backend/career_recommender.py
@@ -11,3 +11,15 @@ def get_career_advice(career_goal: str):
     data = load_careers()
     key = career_goal.lower()
     return data.get(key, None)
+
+from pathlib import Path
+import json
+
+def load_careers():
+    # Path relative to this file
+    current_dir = Path(__file__).parent
+    skills_file = current_dir / "data" / "skills.json"
+    
+    with open(skills_file, "r") as f:
+        data = json.load(f)
+    return data
